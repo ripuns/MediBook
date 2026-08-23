5bb63fc25440fac3c937b2b388539b8bc1da27b5
ripuns <ripunsethia1668@gmail.com>
2026-08-23 11:06:18 +0530

    chore(jobs): add medication reminder job and appointment reminder job at the server startup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,16 +2,22 @@ import 'dotenv/config';
 
 import app from './app';
 import { env } from './config/env';
+import { startAppointmentReminderJob } from './jobs/appointmentReminder.job';
 import { startExpiredHoldCleanupJob } from './jobs/expiredHoldCleanup';
+import { startMedicationReminderJob } from './jobs/medicationReminder.job';
 import { startNotificationRetryJob } from './jobs/notificationRetry';
 
 const port = env.PORT;
 
 const expiredHoldCleanupJob = startExpiredHoldCleanupJob();
 const notificationRetryJob = startNotificationRetryJob();
+const appointmentReminderJob = startAppointmentReminderJob();
+const medicationReminderJob = startMedicationReminderJob();
 
 expiredHoldCleanupJob.start();
 notificationRetryJob.start();
+appointmentReminderJob.start();
+medicationReminderJob.start();
 
 app.listen(port, () => {
   console.log(`MediBook backend listening on http://localhost:${port}`);
